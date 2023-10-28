505961e3e92d084f8f105a0123a9dea25db59cd0
Andreas Finger <andy@mediafinger.com>
2023-10-28 23:32:05 +0200

    Skip Authorization in SessionsController and PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate, only: %i[home error]
-  skip_verify_authorized only: %i[home error] # TODO: home -> guest
+  skip_verify_authorized only: %i[home error]
 
   def home
     redirect_to pictures_path
