1bad5b8080795bb048d47f3a5db931fa33b671ba
Andreas Finger <andy@mediafinger.com>
2023-10-28 21:39:22 +0200

    Authorize TeamsController

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,35 +1,32 @@
 class TeamsController < ApplicationController
-  skip_verify_authorized # TODO: REMOVE!
-  before_action :set_team, only: %i[show edit update destroy]
-
-  # GET /teams
   def index
-    raise AuthError unless current_user.persisted?
+    authorize! current_user, to: :index?, with: TeamPolicy
+
+    # teams = authorized_scope(Team.all, type: :relation, as: :current_team_scope)
 
     @teams = Team.all
   end
 
-  # GET /teams/1
   def show
+    @team = Team.urlsafe_find!(params[:id])
+    authorize! @team
   end
 
-  # GET /teams/new
   def new
-    raise AuthError unless current_user.persisted?
-
     @team = Team.new
+    authorize! @team
   end
 
-  # GET /teams/1/edit
   def edit
-    raise AuthError unless @team == current_team && current_member.owner?
+    @team = Team.urlsafe_find!(params[:id])
+    authorize! @team
   end
 
-  # POST /teams
   def create
     raise AuthError unless current_user.persisted?
 
     @team = Team.new(team_params)
+    authorize! @team
 
     if @team.save
       Member.create!(team: @team, user: current_user, roles: Member::VALID_ROLES)
@@ -41,9 +38,9 @@ class TeamsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /teams/1
   def update
-    raise AuthError unless @team == current_team && current_member.owner?
+    @team = Team.urlsafe_find!(params[:id])
+    authorize! @team
 
     if @team.update(team_params)
       redirect_to @team, notice: "Team was successfully updated."
@@ -52,9 +49,9 @@ class TeamsController < ApplicationController
     end
   end
 
-  # DELETE /teams/1
   def destroy
-    raise AuthError unless @team == current_team && current_member.owner?
+    @team = Team.urlsafe_find!(params[:id])
+    authorize! @team
 
     @team.destroy!
 
@@ -63,12 +60,6 @@ class TeamsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_team
-    @team = Team.urlsafe_find!(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
   def team_params
     params.require(:team).permit(:name)
   end
