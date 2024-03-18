6934944a3d95f252ec0706abd9b488c1f5a9ece2
Andreas Finger <andy@mediafinger.com>
2024-03-18 14:04:46 +0100

    Fix current_team unknown in partials bug

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,4 +71,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include Authentication, type: :controller
+  config.include TeamScope, type: :controller
+  config.include RequestContext, type: :controller
 end
