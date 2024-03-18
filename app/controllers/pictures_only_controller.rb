d12543309b272ae5f571c731c6679bef2f0e220a
Andreas Finger <andy@mediafinger.com>
2024-03-18 16:29:58 +0100

    Switch to active_storage_validations gem (#27)

--- app/controllers/pictures_only_controller.rb
+++ app/controllers/pictures_only_controller.rb
@@ -1,7 +1,7 @@
 class PicturesOnlyController < ApplicationController
   def show
     @picture = Picture.urlsafe_find!(params[:id])
-    authorize! @picture, to: :show?, with: :PicturePolicy
+    authorize! @picture, to: :show?, with: PicturePolicy
 
     render :show, layout: false
   end
