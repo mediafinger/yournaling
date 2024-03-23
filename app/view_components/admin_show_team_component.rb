2f51ce4df59276dd53eaf9d0f00e574eaa2dd133
Andreas Finger <andy@mediafinger.com>
2024-03-23 21:21:19 +0100

    Extract AdminShowTeamComponent

---
+++ app/view_components/admin_show_team_component.rb
@@ -0,0 +1,17 @@
+class AdminShowTeamComponent < ApplicationComponent
+  erb_template <<-ERB
+    <p>
+      <strong>Team:</strong>
+      <%= @team_link_tag %>
+      <i>(<%= @team.yid %>)</i>
+    </p>
+  ERB
+
+  def initialize(team:)
+    @team = team
+  end
+
+  def before_render
+    @team_link_tag = link_to(@team.name, admin_team_url(@team))
+  end
+end
