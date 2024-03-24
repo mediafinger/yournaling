ee4c32c1ef98505f9c72d2ebc882cf1289c093c4
Andreas Finger <andy@mediafinger.com>
2024-03-24 09:11:00 +0100

    Persist RecordHistory in db:seeds (#37)

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,24 +12,33 @@ end
 
 # create records
 
-andy = User.create!(email: "andy@example.com", password: "foobar1234", name: "Andy Finger", role: "admin")
-dodo = User.create!(email: "dodo@example.com", password: "foobar1234", name: "Dodo Finger")
-user = User.create!(email: "user@example.com", password: "foobar1234", name: "User Account")
+andy = User.new(email: "andy@example.com", password: "foobar1234", name: "Andy Finger", role: "admin")
+dodo = User.new(email: "dodo@example.com", password: "foobar1234", name: "Dodo Finger")
+user = User.new(email: "user@example.com", password: "foobar1234", name: "User Account")
+User.create_with_history(record: andy, history_params: { team: Team.new(yid: :none), user: User.new(yid: :admin) })
+User.create_with_history(record: dodo, history_params: { team: Team.new(yid: :none), user: User.new(yid: :admin) })
+User.create_with_history(record: user, history_params: { team: Team.new(yid: :none), user: User.new(yid: :admin) })
 
-van = Team.create!(name: "RanTanVan")
-team = Team.create!(name: "team two")
+van = Team.new(name: "RanTanVan")
+team = Team.new(name: "team two")
+Team.create_with_history(record: van, history_params: { team: van, user: User.new(yid: :admin) })
+Team.create_with_history(record: team, history_params: { team: team, user: User.new(yid: :admin) })
 
-van_owner = Member.create!(user: andy, team: van, roles: %w[owner publisher])
-van_editor = Member.create!(user: dodo, team: van, roles: %w[manager editor])
+van_owner = Member.new(user: andy, team: van, roles: %w[owner publisher])
+van_editor = Member.new(user: dodo, team: van, roles: %w[manager editor])
+team_owner = Member.new(user:, team:, roles: %w[owner editor])
+Member.create_with_history(record: van_owner, history_params: { team: van, user: andy })
+Member.create_with_history(record: van_editor, history_params: { team: van, user: andy })
+Member.create_with_history(record: team_owner, history_params: { team: team, user: user })
 
-team_owner = Member.create!(user:, team:, roles: %w[owner editor])
+van_pic = FactoryBot.build(:picture, team: van)
+Picture.create_with_history(record: van_pic, history_params: { team: van, user: andy })
 
-van_pic = FactoryBot.create(:picture, team: van)
+loc = Location.new(country_code: "es", address: "N-340, Km 79.3, 11380 Tarifa, Cádiz", lat: "36.0523", long: "-5.6487", name: "Tarifa - La Peña", url: "https://www.google.com/maps?q=36.0523,-5.6487", team: van)
+Location.create_with_history(record: loc, history_params: { team: van, user: andy })
 
-loc = Location.create!(country_code: "es", address: "N-340, Km 79.3, 11380 Tarifa, Cádiz", lat: "36.0523", long: "-5.6487", name: "Tarifa - La Peña", url: "https://www.google.com/maps?q=36.0523,-5.6487", team: van)
-
-weblink = Weblink.create!(name: "Yournaling", url: "www.yournaling.com", description: "Your Journaling", team: van)
-
+weblink = Weblink.new(name: "Yournaling", url: "www.yournaling.com", description: "Your Journaling", team: van)
+Weblink.create_with_history(record: weblink, history_params: { team: van, user: andy })
 
 # TODO: create memory
 # TODO: create chronicle
