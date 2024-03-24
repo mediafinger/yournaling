c6f49ac4efe07212b746b1831c5055397017c877
Andreas Finger <andy@mediafinger.com>
2024-03-24 02:57:18 +0100

    Extract SearchResultsComponent (#35)

---
+++ app/view_components/search_results_component.rb
@@ -0,0 +1,29 @@
+class SearchResultsComponent < ApplicationComponent
+  erb_template <<-ERB
+    <% if @results.present? %>
+      <ul>
+      <% @results.each do |result| %>
+          <li>
+            <%= @record_links[result["searchable_id"]] %>
+            -
+            <%# result["content"] %>
+            <i>(updated_at: <%= DateTime.parse(result["updated_at"]).to_formatted_s(:db) %>)</i>
+          </li>
+      <% end %>
+      </ul>
+    <% end %>
+  ERB
+
+  def initialize(results:)
+    @results = results
+  end
+
+  def before_render
+    @record_links = @results&.each_with_object({}) do |result, hash|
+      record = ApplicationRecordYidEnabled.fynd(result["searchable_id"])
+      link_text = "#{record.class.name}: #{record.name}"
+      link_path = send(:"#{record.class.name.tableize.singularize}_url", record)
+      hash[result["searchable_id"]] = link_to(link_text, link_path)
+    end
+  end
+end
