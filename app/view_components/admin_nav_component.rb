1f00bbc3e049072cbd5887ded363548dbb0974e0
Andreas Finger <andy@mediafinger.com>
2024-03-24 03:06:05 +0100

    Tidy up nav bar links (#35)

--- app/view_components/admin_nav_component.rb
+++ app/view_components/admin_nav_component.rb
@@ -1,5 +1,11 @@
 class AdminNavComponent < ApplicationComponent
   erb_template <<-ERB
+    <% if current_user&.admin? %>
+    <ul>
+      <li><%= link_to "Leave Admin Area", "/" %></li>
+    </ul>
+    <% end %>
+
     <%= render ApplicationNavLinksComponent.new(link_sections: @sections, scope: "admin") %>
 
     <%= render ApplicationNavActionsComponent.new(actions_for: @sections, scope: "admin") %>
