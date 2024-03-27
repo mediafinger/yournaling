22f8ee4f817464e9a2913c9931203df2637d1031
Andreas Finger <andy@mediafinger.com>
2024-03-27 22:39:25 +0100

    Make team/members functional (#43)

--- app/controllers/teams/members_controller.rb
+++ app/controllers/teams/members_controller.rb
@@ -2,10 +2,14 @@ module Teams
   class MembersController < AppTeamsController
     def index
       @members = records_scope(Member.with_includes)
+
+      render "teams/members/index"
     end
 
     def show
       @member = record(Member.with_includes, params[:id])
+
+      render "teams/members/show"
     end
   end
 end
