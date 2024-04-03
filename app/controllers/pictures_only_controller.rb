3e19951359009801729b521862d29ba052c12d49
Andreas Finger <andy@mediafinger.com>
2024-04-03 15:07:59 +0100

    Remove no longer needed controllers and views (#43)

--- app/controllers/pictures_only_controller.rb
+++
@@ -1,8 +0,0 @@
-class PicturesOnlyController < ApplicationController
-  def show
-    @picture = Picture.urlsafe_find!(params[:id])
-    authorize! @picture, to: :show?, with: PicturePolicy
-
-    render :show, layout: false
-  end
-end
