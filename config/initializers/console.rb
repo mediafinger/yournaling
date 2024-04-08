8c3717454b1933cae96f471ea14ebc00f92e1a52
Andreas Finger <andy@mediafinger.com>
2024-04-09 00:08:55 +0100

    Make rubocop happy

--- config/initializers/console.rb
+++ config/initializers/console.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Rails/Output
 Rails.application.console do
   if AppConf.is?(:environment, :development)
     @andy = User.find_by(email: "andy@example.com")
@@ -6,3 +7,4 @@ Rails.application.console do
     puts "Yournaling development console - initialized @andy and @van - don't forget about `show_cmds`"
   end
 end
+# rubocop:enable Rails/Output
