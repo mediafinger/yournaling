f264b8846312863cb41b9bc44dad249f83f85b54
Andreas Finger <andy@mediafinger.com>
2024-04-08 15:09:17 +0100

    Add rails console initializer

---
+++ config/initializers/console.rb
@@ -0,0 +1,8 @@
+Rails.application.console do
+  if AppConf.is?(:environment, :development)
+    @andy = User.find_by(email: "andy@example.com")
+    @van  = Team.find_by(name: "RanTanVan")
+
+    puts "Yournaling development console - initialized @andy and @van - don't forget about `show_cmds`"
+  end
+end
