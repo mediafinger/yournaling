0939f4fe65ab7bdb1be23b8a9a4960ec6c6beefe
Andreas Finger <andy@mediafinger.com>
2024-04-24 21:23:28 +0200

    FInally solve config.fixture_path deprecation (#48)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,8 +41,11 @@ FactoryBot::SyntaxRunner.class_eval do
 end
 
 RSpec.configure do |config|
+  # To get the full backtrace of deprectations
+  # config.raise_errors_for_deprecations!
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{Rails.root.join('spec/fixtures')}"
+  config.fixture_paths = [ "#{Rails.root.join('spec/fixtures')}" ]
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
