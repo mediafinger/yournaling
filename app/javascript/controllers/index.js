3053314e8145a5e8890c1e9356322435f00eda31
Andreas Finger <andy@mediafinger.com>
2024-11-05 09:05:46 +0200

    Use absolute path for importmap

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,4 @@
-// Import and register all your controllers from the importmap under controllers/*
-
+// Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
 
 // Eager load all controllers defined in the import map under controllers/**/*_controller
